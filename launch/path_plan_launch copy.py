45ec2b42d7746eb4560f053ea9ce844d7080cd48
lidarmansiwon <kimsanmaro@naver.com>
2024-09-12 10:50:26 +0900

    Update0912

--- launch/path_plan_launch copy.py
+++ launch/path_plan_launch copy.py
@@ -10,32 +10,7 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.substitutions import FindPackageShare
 from launch.event_handlers import OnProcessStart, OnProcessExit
 
-def select_pcd_file():
-    folder_path = '/home/macroorin3/slamMAP'
-    pcd_files = [f for f in os.listdir(folder_path) if f.endswith('.pcd')]
-
-    if not pcd_files:
-        print("\033[31mNo .pcd files found in the folder.\033[0m")
-        return None
-
-    print("\033[32mSelect a pcd file:\033[0m")
-    for idx, file_name in enumerate(pcd_files):
-        print(f"\033[33m{idx + 1}: {file_name}\033[0m")
-
-    selected_idx = int(input("\033[32mEnter the number of the file: \033[0m")) - 1
-
-    if 0 <= selected_idx < len(pcd_files):
-        return os.path.join(folder_path, pcd_files[selected_idx])
-    else:
-        print("\033[31mInvalid selection.\033[0m")
-        return None
-    
 def generate_launch_description():
-    
-    pcd_file = select_pcd_file()
-
-    if not pcd_file:
-        raise RuntimeError("No valid .pcd file selected.")
     
     include_pointcloud_to_grid = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -53,8 +28,8 @@ def generate_launch_description():
     
     path_plan = Node(
         package="rviz_click_path_plan",
-        executable="record_path_generator",
-        name="record_path_generator",
+        executable="click_planner",
+        name="click_planner",
         output="screen",
         emulate_tty=True,
     )
